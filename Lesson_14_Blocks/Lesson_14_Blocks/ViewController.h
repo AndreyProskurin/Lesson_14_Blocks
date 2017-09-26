c6a7eb3c693ca12d5652577313856ebd5e4f42d7
Andrey Proskurin <andrewproskurin@gmail.com>
2017-09-26 19:06:49 +0300

    Empty single view app has been added.

---
+++ Lesson_14_Blocks/Lesson_14_Blocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_14_Blocks
+//
+//  Created by Andrey Proskurin on 26.09.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
